de6b51862c6fd0b265200f15bff4bf06f34de047
Bushra1995 <bushra95ga@gmail.com>
2023-05-05 22:20:13 +0300

    60% of Checkout page has been finished

--- stationery/src/components/LandingPage/LandingPage.jsx
+++ stationery/src/components/LandingPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./Hero";
 import Content from "./Content";
 import "./landingpage.css";
+import Checkout from "../Checkout/Checkout";
 
 export default function LandingPage() {
   return (
     <>
       <Hero />
       <Content />
+      <Checkout />
     </>
   );
 }
