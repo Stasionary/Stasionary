caca5b100b55d365571bed622661aa6c34cf6348
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-05-05 19:11:16 +0300

    Added new file

--- stationery/src/App.js
+++ stationery/src/App.js
@@ -1,10 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-
-import "./App.css";
+import Layout from "./components/Navbar/Layout";
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
-  return <Navbar />;
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          // other routes here
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
