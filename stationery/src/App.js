7d683b9f43cfe7eb733e5dcfd0d6208118783bea
MajdFoudeh <majdfoude@gmail.com>
2023-05-06 16:08:22 +0300

    cart page and product details

--- stationery/src/App.js
+++ stationery/src/App.js
@@ -1,17 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from "./components/Navbar/Layout";
 import LandingPage from "./components/LandingPage/LandingPage";
+import { Cart } from "./components/cart/Cart";
+import { Checkout } from "./components/checkout/checkout";
+import { ProductDetails } from "./components/product details/productDetails";
 
 function App() {
   return (
+
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           // other routes here
+          <Route path="cartPage" element={<Cart />} />
+          <Route path="checkoutPage" element={<Checkout/>}/>
+          <Route path="productDetailsPage" element={<ProductDetails/>}/>
         </Routes>
       </Layout>
     </Router>
+
   );
 }
 
