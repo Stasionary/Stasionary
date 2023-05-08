8f087c492e264175b3c12328f7ef920b6ad672eb
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-05-08 09:46:33 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import { isLoginContext } from "./loginContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Navbar/Layout";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Register from "./components/Login-sigup-pages/Register";
 import Login from "./components/Login-sigup-pages/Login";
 import ProductsPage from "./components/ProductsPage/ProductsPage";
-// import Checkout from "./components/Checkout/Checkout";
 import { ProductDetails } from "./components/product details/productDetails";
-
-import Cart from '../src/components/cart/Cart'
 import { ContactUs } from "./components/Contact us/ContactUs";
 import { AboutUs } from "./components/About Us/AboutUs";
 import Cart from "../src/components/cart/Cart";
@@ -15,36 +13,32 @@ import { createContext, useState } from "react";
 export const ItemContext = createContext([]);
 
 
-
-import { isLoginContext } from "./loginContext";
-
- 
 function App() {
 
-const [isLogin , setIsLogin] = useState(false)
+  const [isLogin, setIsLogin] = useState(false)
   const [item, setItem] = useState([]);
   return (
-    <isLoginContext.Provider value={{isLogin , setIsLogin  }}>
+    <isLoginContext.Provider value={{ isLogin, setIsLogin }}>
 
-    <Router>
-      <Layout>
+      <Router>
+        <Layout>
 
-        <ItemContext.Provider value={{ item, setItem }}>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/productsPage" element={<ProductsPage />} />
-            <Route path="cartPage" element={<Cart />} />
-              <Route path="aboutAsPage" element={<AboutUs/>}/>
-          <Route path="contactUsPage" element={<ContactUs/>}/>
-            <Route path="/ProductDetailsPage" element={<ProductDetails />} />
-            {/* <Route path="cartPage" element={<Cart />} /> */}
-          </Routes>
-        </ItemContext.Provider>
+          <ItemContext.Provider value={{ item, setItem }}>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/productsPage" element={<ProductsPage />} />
+              <Route path="cartPage" element={<Cart />} />
+              <Route path="aboutAsPage" element={<AboutUs />} />
+              <Route path="contactUsPage" element={<ContactUs />} />
+              <Route path="/ProductDetailsPage" element={<ProductDetails />} />
+              {/* <Route path="cartPage" element={<Cart />} /> */}
+            </Routes>
+          </ItemContext.Provider>
 
-      </Layout>
-    </Router>
+        </Layout>
+      </Router>
     </isLoginContext.Provider>
   );
 }
