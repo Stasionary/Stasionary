e2c12c3c1ad099adc7541a933068eab36d998564
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-05-08 16:58:34 +0300

    Added categories filtering

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -19,17 +19,13 @@ const ProductsPage = () => {
   const navigate = useNavigate();
   const { item, setItem } = useContext(ItemContext);
   const { counter, setCounter } = useContext(counterContext);
-
+  const [abc, setAbc] = useState(Products);
   function AddToCart(event) {
     event.preventDefault();
     const selectedProduct = Products.find(
       (product) => product.id === event.target.id
     );
-    function counter(event) {
-      const selectedProduct =
-        Products.filter((product) => product.id === event.target.id) + 1;
-      console.log(selectedProduct);
-    }
+    setCounter(counter + 1);
     // Retrieve previous items from local storage
     const storedItems = JSON.parse(localStorage.getItem("newItem")) || [];
     // Add the new item to the array of stored items
@@ -53,8 +49,18 @@ const ProductsPage = () => {
     sessionStorage.setItem("newItem", JSON.stringify(array));
     navigate("/ProductDetailsPage");
   }
-
-  const Cards = Products.map((product, index) => {
+  let filteredCategory;
+  let filteredCategoryAfterClicking = Products;
+  function filterCategory(event) {
+    filteredCategory = Products.filter(
+      (product) => product.category === event.target.id
+    );
+    console.log(filteredCategory);
+    setAbc(filteredCategory);
+    filteredCategoryAfterClicking = filteredCategory;
+  }
+  // abc = cards ? setAbc(Cards) : setAbc(CardsFiltered);
+  const Cards = abc.map((product, index) => {
     return (
       <div
         key={index}
@@ -107,6 +113,60 @@ const ProductsPage = () => {
       </div>
     );
   });
+  const CardsFiltered = abc.map((product, index) => {
+    return (
+      <div
+        key={index}
+        className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col bg-primary m-5 rounded-md"
+      >
+        <a href="#">
+          <img
+            id={product.id}
+            onClick={AddToProductDetails}
+            className="hover:grow hover:shadow-lg"
+            src={product.img}
+          />
+          <div className="flex items-center mt-5 justify-between">
+            <p className="">{product.title}</p>
+            <p className="ml-20  text-gray-900">{product.price} JD</p>
+          </div>
+
+          <div className="flex justify-around mt-3">
+            <button
+              className="btn btn-outline "
+              id={product.id}
+              onClick={AddToCart}
+            >
+              Add to cart
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="file: ml-2 h-6 w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </button>
+            <button
+              className="btn btn-outline-blue"
+              id={product.id}
+              onClick={AddToProductDetails}
+            >
+              {" "}
+              Details
+            </button>
+          </div>
+        </a>
+      </div>
+    );
+  });
+  console.log(CardsFiltered);
   return (
     <div>
       <>
@@ -124,49 +184,63 @@ const ProductsPage = () => {
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="Paper Products"
                 >
                   Paper Products
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  onClick={filterCategory}
+                  id="Desk Accessories"
                 >
                   Desk Accessories
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="Art Supplies"
                 >
                   Art Supplies
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="School Supplies"
                 >
                   School Supplies
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="Presentation"
                 >
                   Presentation Supplies
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="Calendars and Planners"
                 >
                   Calendars and Planners
                 </a>
                 <a
                   href="#"
                   className="block font-medium text-gray-500 dark:text-gray-300 hover:underline"
+                  onClick={filterCategory}
+                  id="Filing and Organization"
                 >
                   Filing and Organization
                 </a>
               </div>
               <section className="bg-white py-8 ">
                 <div className="container mx-auto flex items-center flex-wrap pb-12">
-                  {Cards}
+                  {abc === false ? Cards : CardsFiltered}
                 </div>
               </section>
             </div>
